e69e945723b032c2e6a6edf4c79ef692cb47d275
BlackBeltHacker <114818497+BlackBeltHacker@users.noreply.github.com>
2023-02-24 03:10:26 +0000

    sorting out the entry box

--- src/components/EntryCard.jsx
+++ src/components/EntryCard.jsx
@@ -3,22 +3,39 @@ import React from 'react';
 function EntryCard() {
     return (
         <>
+            {/* Entry content */}
             <div className="entry-card col-9">
-                <div className="title-box">
-                    <div className="date-holder">
-                        <h6>Fri 24 Feb 02:46</h6>
+                <div className="content-box">
+                    <div className="title-box">
+                        <div className="date-holder">
+                            <h6>Fri 24 Feb 02:46</h6>
+                        </div>
+
+                        <div className="title-holder">
+                            <h5>Todays Placeholder Entry</h5>
+                        </div>
                     </div>
 
-                    <div className="title-holder">
-                        <h5>Todays Placeholder Entry</h5>
+                    <div className="entry-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci fugit esse, sit quo voluptate non
+                            fuga harum recusandae, sed ipsum quidem fugiat
+                            quaerat deserunt nam expedita. Eaque sed in ratione?
+                        </p>
                     </div>
                 </div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Adipisci fugit esse, sit quo voluptate non fuga harum
-                    recusandae, sed ipsum quidem fugiat quaerat deserunt nam
-                    expedita. Eaque sed in ratione?
-                </p>
+                {/* buttons tags and lower half */}
+
+                <div className="tag-holder">
+                    
+
+                </div>
+
+                <div className="button-box">
+
+
+                </div>
             </div>
         </>
     );

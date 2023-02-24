3df7732af1054e6ed3d0e22d5e6a1f24a9a3d36b
BlackBeltHacker <114818497+BlackBeltHacker@users.noreply.github.com>
2023-02-24 01:07:30 +0000

    Doing Row

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import Textfield from "./components/Textfield";
-import JournalEntry from "./components/JournalEntry";
-import { useState } from "react";
+import React from "react";
 
+import Row from "./components/Row";
 
-
-function App(props) {
-    const [journalEntries, setJournalEntries] = useState(JSON.parse(localStorage.getItem('journal')) || [])
-
+function App() {
+    return (<>
     
-
-    return (
-    <>
-    
-        <Textfield setJournalEntries={setJournalEntries}/>
-        {journalEntries.map( (entry, index) => {
-            return(
-
-                <JournalEntry key={index} entry={entry}/>
-
-            )
-        })}
-        
-
-    </>
-    )
+    </>);
 }
 
 export default App;
---
+++ src/components/Row.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Row() {
+
+    
+    return (
+
+        <>
+            
+        </>
+
+    )
+}
+
+export default Row;

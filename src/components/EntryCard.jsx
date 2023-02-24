9d9e7d1f21574b0e8390b7bc299f7e7e9c83489a
BlackBeltHacker <114818497+BlackBeltHacker@users.noreply.github.com>
2023-02-24 02:52:47 +0000

    Thar she grows?

--- src/components/EntryCard.jsx
+++ src/components/EntryCard.jsx
@@ -5,8 +5,13 @@ function EntryCard() {
         <>
             <div className="entry-card col-9">
                 <div className="title-box">
-                    <h6>time and date</h6>
-                    <h5>Title placeholder</h5>
+                    <div className="date-holder">
+                        <h6>Fri 24 Feb 02:46</h6>
+                    </div>
+
+                    <div className="title-holder">
+                        <h5>Todays Placeholder Entry</h5>
+                    </div>
                 </div>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
